b4b18b8f20aae61eb3e27c6777fbcdf59aeb104d
Bruno Santos <124303374+brunovcsantos@users.noreply.github.com>
2023-07-17 19:20:33 -0300

    create news EXs

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} KM/h!`)
+if (vel > 60){  //condição simples
+    console.log(`Você ultrapassou o limite permitido. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
