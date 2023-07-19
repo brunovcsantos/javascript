d85e61b165e8d195d0f905a228658a77e632efc7
Bruno Santos <124303374+brunovcsantos@users.noreply.github.com>
2023-07-19 16:09:59 -0300

    novas pastas para o ex

--- exercicios/aula012/ex012.js
+++ exercicios/aula012/ex012.js
@@ -4,7 +4,7 @@ var minuto = agora.getMinutes()
 console.log(`Agora são ${hora} : ${minuto} horas.`)
 if (hora >= 0 && hora < 6) {
     console.log('Boa madrugada!')
-} else if (hora <= 12) {
+} else if (hora < 12) {
     console.log('Bom dia!')
 } else if (hora <= 18) {
     console.log('Boa tarde!')
